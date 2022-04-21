a91f84ef41ed0615a8574fd93f4e7bc236683a0c
yadielCruzado <yadiel.cruzado@upr.edu>
2022-04-20 21:27:48 -0400

    a

--- showcart.php
+++ showcart.php
@@ -3,15 +3,39 @@ include('template/header.php');
 ?>
 
 <br>
-<h3> lista del carrito </h3>
-<table>
-    <tbody>
-        <th width="40%" >Descripcion</th>
-        <th width="15%">cantidad</th>
-        <th width="20%">Precio</th>
-        <th width="40%">Total</th>
-        <th width="5%">--</th>
-    </tbody>
-</table>
+<div class="cart">
+    <h3> lista del carrito </h3>
+    <table>
+        <tbody>
+            <th width="40%" >Descripcion</th>
+            <th width="15%">cantidad</th>
+            <th width="20%">Precio</th>
+            <th width="40%">Total</th>
+            <th width="5%">--</th>
+        </tbody>
+        <tbody>
+            <th width="40%" >Traxxas Sledge RTR 6S 4WD Electric Monster Truck (Blue) w/VXL-6s ESC & TQi 2.4GHz Radio</th>
+            <th width="15%">1</th>
+            <th width="20%">$799.95</th>
+            <th width="40%">$799.95</th>
+            <th width="5%">--</th>
+        </tbody>
+        <tbody>
+            <th width="40%" >Tamiya Toyota Land Cruiser 40 Pickup w/Tracks (GF-01T)</th>
+            <th width="15%">1</th>
+            <th width="20%">$327</th>
+            <th width="40%">$1,126.95</th>
+            <th width="5%">--</th>
+        </tbody>
+        <tbody>
+            <th width="40%">Tamiya MAN TGX 4x2 XLX 1/14 Semi Kit (Red)</th>
+            <th width="15%">1</th>
+            <th width="20%">$638</th>
+            <th width="40%">17</th>
+            <th width="5%">--</th>
+        </tbody>
+    </table>    
+</div>
+
 
 <?php include('template/footer.php');  ?>
